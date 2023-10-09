072a2cdeb627d0438662540bafb1d3d24f4287dd
cltts <clttsousa@hotmail.com>
2023-10-09 16:26:33 -0300

    Service e registro de componentes

--- src/main/java/com/cleitonsousa/workshop/config/TestConfig.java
+++ src/main/java/com/cleitonsousa/workshop/config/TestConfig.java
@@ -21,8 +21,10 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Cleiton Sousa", "cleiton@gmail.com", "999117145", "123456");
+		User u4 = new User(null, "Amanda Estevam", "amanda@gmail.com", "912345115", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 	}
 
 }
