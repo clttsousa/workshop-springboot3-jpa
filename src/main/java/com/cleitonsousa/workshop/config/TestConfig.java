2bc45de26fc34417f019f6ce97eb6e3f25343ae0
cltts <clttsousa@hotmail.com>
2023-10-16 14:16:03 -0300

    enum OrderStatus

--- src/main/java/com/cleitonsousa/workshop/config/TestConfig.java
+++ src/main/java/com/cleitonsousa/workshop/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.cleitonsousa.workshop.entities.Order;
 import com.cleitonsousa.workshop.entities.User;
+import com.cleitonsousa.workshop.entities.enuns.OrderStatus;
 import com.cleitonsousa.workshop.repositories.OrderRepository;
 import com.cleitonsousa.workshop.repositories.UserRepository;
 
@@ -31,11 +32,11 @@ public class TestConfig implements CommandLineRunner {
 		User u4 = new User(null, "Amanda Estevam", "amanda@gmail.com", "912345115", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), u3);
-		Order o4 = new Order(null, Instant.parse("2022-02-22T15:21:22Z"), u4);
-		Order o5 = new Order(null, Instant.parse("2023-09-20T15:21:22Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.SHIPPED, u2);
+		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
+		Order o4 = new Order(null, Instant.parse("2022-02-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u4);
+		Order o5 = new Order(null, Instant.parse("2023-09-20T15:21:22Z"), OrderStatus.CANCELED, u3);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4,o5));
