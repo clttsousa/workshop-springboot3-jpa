f828747966ba7b7308d0bfa889cb128db85e973a
cltts <clttsousa@hotmail.com>
2023-10-24 15:12:26 -0300

    Exception no findById, pra caso não exista o id informado

--- src/main/java/com/cleitonsousa/workshop/services/UserService.java
+++ src/main/java/com/cleitonsousa/workshop/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cleitonsousa.workshop.entities.User;
 import com.cleitonsousa.workshop.repositories.UserRepository;
+import com.cleitonsousa.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById (Long id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
